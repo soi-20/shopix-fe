2eef3e041cc5f01032a715d471f4cb66d3800cac
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2024-05-26 02:28:07 +0530

    fix: search polling

--- app/api/getSearchResult/[id]/route.ts
+++ app/api/getSearchResult/[id]/route.ts
@@ -1,29 +1,35 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-    try {
-        const url = new URL(req.url);
-        const taskId = url.searchParams.get("id");
-
-        if (!taskId) {
-            return NextResponse.json({ error: "Missing task ID" }, { status: 400 });
-        }
-
-        const response = await fetch(`https://experiments-testing.azurewebsites.net/task-status/${taskId}`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return NextResponse.json(data);
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    if (!params.id) {
+      return NextResponse.json({ error: "Missing task ID" }, { status: 400 });
     }
+
+    const response = await fetch(
+      `https://experiments-testing.azurewebsites.net/task-status/${params.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
