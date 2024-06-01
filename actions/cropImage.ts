387369fe01b33388958ce58ed93811f5f9f1cc41
Mito <aaditya.uwwu@gmail.com>
2024-06-01 19:28:19 +0530

    pushing new changes

--- actions/cropImage.ts
+++ actions/cropImage.ts
@@ -31,15 +31,10 @@ export async function canvasPreview(image: any, crop: any, scale = 1, rotate = 0
 
   ctx.save();
 
-  // 5) Move the crop origin to the canvas origin (0,0)
   ctx.translate(-cropX, -cropY);
-  // 4) Move the origin to the center of the original position
   ctx.translate(centerX, centerY);
-  // 3) Rotate around the origin
   ctx.rotate(rotateRads);
-  // 2) Scale the image
   ctx.scale(scale, scale);
-  // 1) Move the center of the image to the origin (0,0)
   ctx.translate(-centerX, -centerY);
   ctx.drawImage(
     image,
