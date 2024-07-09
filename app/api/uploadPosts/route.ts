2ff89af7bbab367688eb603b78f4ff0d68c87f44
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-09 19:03:50 +0530

    API for Database queries for Instagram

---
+++ app/api/uploadPosts/route.ts
@@ -0,0 +1,40 @@
+// pages/api/upload.js
+import pool from "@/lib/db";
+import { NextResponse } from "next/server";
+
+interface Body {
+  post: string;
+  postData: string;
+}
+
+export async function POST(req: Request, res: NextResponse) {
+  const { post, postData }: Body = await req.json();
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO Posts (posts, postData) VALUES ($1, $2) RETURNING *",
+      [JSON.stringify(post), JSON.stringify(postData)]
+    );
+    return NextResponse.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error inserting data", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+// Fetch all data
+export async function GET(req: Request, res: NextResponse) {
+  try {
+    const result = await pool.query("SELECT * FROM Posts");
+    return NextResponse.json(result.rows);
+  } catch (error) {
+    console.error("Error fetching data", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
