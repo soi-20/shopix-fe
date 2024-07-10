efc35b78cb2759ba1413f0bb46b3cde713c48a53
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-10 11:56:24 +0530

    Modified Config for db

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,14 @@
 // lib/db.js
 import { Pool } from "pg";
+import process from "process";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Instagram",
-  password: "postgres$1008",
+  user: "soi",
+  host: "shoppin-db.postgres.database.azure.com",
+  database: "InstaPosts",
+  password: process.env.POSTGRES_PASSWORD,
   port: 5432,
+  ssl: true,
 });
 
 export default pool;
