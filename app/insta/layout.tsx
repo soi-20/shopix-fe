172574f8c3700275b13dadb0edca36d55da038ae
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-10 12:38:12 +0530

    Fixed footer

--- app/insta/layout.tsx
+++ app/insta/layout.tsx
@@ -6,9 +6,9 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       {/* <Navbar /> */}
-      {children}
+      <div className="flex-grow">{children}</div>
       <div className="text-center mb-8 text-sm text-[#444444]">
         Made with ❤ in India
       </div>
