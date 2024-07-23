0c1c2937be644efcf258ecee0383d370245e457f
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-23 19:18:26 +0530

    Returning session as well

--- lib/auth/checkIsAuthenticated.ts
+++ lib/auth/checkIsAuthenticated.ts
@@ -4,8 +4,6 @@ import { auth } from "./authConfig";
 
 export const checkIsAuthenticated = async () => {
   const session = await auth();
-  if (session) {
-    return true;
-  }
-  return false;
+  const isAuthenticated = !!session;
+  return { isAuthenticated, session };
 };
