d16514c4d2e5ae74ade8dcf1b8e8e50138e5e264
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-23 18:38:01 +0530

    Added authorization key (to get a refresh token)

--- lib/auth/authConfig.ts
+++ lib/auth/authConfig.ts
@@ -18,7 +18,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      allowDangerousEmailAccountLinking: true, // Allow automatic linking of users table to accounts table in database - not dangerous when used with OAuth providers that already perform email verification (like Google)
+      allowDangerousEmailAccountLinking: true,
+      authorization: { params: { access_type: "offline", prompt: "consent" } },
+      // Allow automatic linking of users table to accounts table in database - not dangerous when used with OAuth providers that already perform email verification (like Google)
     }),
   ],
   callbacks: {
