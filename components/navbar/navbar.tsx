2c14632d380f10aab8d4ea49c5e137c24c15e2d2
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-23 18:12:18 +0530

    Rendering navbar conditionally

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,64 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeToggle } from "../theme-toggle/theme-toggle";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import { checkIsAuthenticated } from "@/lib/auth/checkIsAuthenticated";
+import { handleSignOut as signOutAction } from "@/lib/auth/signoutServerAction";
 
 const Navbar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authStatus = await checkIsAuthenticated();
+      setIsAuthenticated(authStatus);
+    };
+
+    checkAuth();
+  }, []);
+
+  const handleSignIn = () => {
+    router.push("/auth/sign-in");
+  };
+
+  const handleSignOut = async () => {
+    await signOutAction();
+    router.push("/auth/sign-in");
+  };
+
+  // Display a loading state while checking authentication
+  if (isAuthenticated === null) {
+    return (
+      <div className="flex justify-between items-center p-4">
+        <Link href="/">
+          <p className="text-xl cursor-pointer text-center font-bold text-slate-500">
+            shopix
+          </p>
+        </Link>
+        <div className="flex items-center gap-8">
+          <ThemeToggle />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex justify-between items-center flex-row p-4">
+    <div className="flex justify-between items-center p-4">
       <Link href="/">
         <p className="text-xl cursor-pointer text-center font-bold text-slate-500">
           shopix
         </p>
       </Link>
-      <ThemeToggle/>
+      <div className="flex items-center gap-8">
+        {isAuthenticated ? (
+          <Button onClick={handleSignOut}>Sign Out</Button>
+        ) : (
+          <Button onClick={handleSignIn}>Sign In</Button>
+        )}
+        <ThemeToggle />
+      </div>
     </div>
   );
 };
