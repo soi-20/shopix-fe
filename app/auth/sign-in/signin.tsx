9fb9556c3e86eb80171121aee27d8ae9a9ad9201
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-24 09:48:35 +0530

    Modified sign in commponent

--- app/auth/sign-in/signin.tsx
+++ app/auth/sign-in/signin.tsx
@@ -5,15 +5,15 @@ import { handleGoogleSignIn } from "@/lib/auth/googleSignInServerAction";
 
 export const SignInPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[hsl(0,0%,97.5%)]">
-      <div className="w-80 p-8 bg-white rounded-xl shadow-lg text-center">
-        <h2 className="text-2xl font-semibold mb-4">Sign In</h2>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="w-96 p-8 bg-white rounded-3xl shadow-2xl text-center">
+        <h2 className="text-3xl font-bold text-gray-800 mb-6">Sign in</h2>
         <div className="flex flex-col items-center">
           <button
-            className="flex items-center justify-center w-full p-4 text-md font-bold border rounded-xl bg-transparent hover:bg-[hsl(0,0%,97.5%)] focus:outline-none"
+            className="flex items-center justify-center w-full p-4 text-lg font-semibold border border-gray-300 rounded-full bg-white hover:bg-gray-100 transition-colors duration-300 focus:outline-none shadow-md"
             onClick={() => handleGoogleSignIn()}
           >
-            <FcGoogle className="mr-5 w-6 h-6" />
+            <FcGoogle className="mr-4 w-7 h-7" />
             Sign in with Google
           </button>
         </div>
