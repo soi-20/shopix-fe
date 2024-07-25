b82cb520ada96533b672cf7ea8e61082948d1041
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-25 12:50:12 +0530

    Added schema for database

--- database/postgres-schema-auth.sql
+++ database/postgres-schema-auth.sql
@@ -24,3 +24,33 @@ CREATE TABLE IF NOT EXISTS accounts (
   token_type TEXT,
   FOREIGN KEY ("userId") REFERENCES users(id) ON DELETE CASCADE
 );
+
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    image TEXT NOT NULL,
+    rating FLOAT,
+    price DECIMAL(10, 2) NOT NULL,
+    logo TEXT,
+    link TEXT,
+    site_id UUID,
+    site_name VARCHAR(255),
+    brand_name VARCHAR(255),
+    delivery VARCHAR(255),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+CREATE TABLE IF NOT EXISTS wishlist (
+  id UUID PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  product_id UUID NOT NULL,
+  added_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+  UNIQUE (user_id, product_id)  -- Ensure that each user can only have one entry per product
+);
