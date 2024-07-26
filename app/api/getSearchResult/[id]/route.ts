993ce26a76da6f1913a100ddf94f5050c0ae60b9
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-26 16:21:19 +0530

    Added isAuthenticated check to get userID (fetching from search table)

--- app/api/getSearchResult/[id]/route.ts
+++ app/api/getSearchResult/[id]/route.ts
@@ -1,23 +1,30 @@
 import { NextResponse } from "next/server";
 import { auth_pool as pool } from "@/lib/db";
+import { checkIsAuthenticated } from "@/lib/auth/checkIsAuthenticated";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
+  const { isAuthenticated, session } = await checkIsAuthenticated();
+  let userId = null;
+  if (isAuthenticated) {
+    userId = session?.user?.id;
+  }
+
   try {
     if (!params.id) {
       return NextResponse.json({ error: "Missing Search ID" }, { status: 400 });
     }
 
     const result = await pool.query(
-      "SELECT json_response FROM search WHERE search_id = $1",
-      [params.id]
+      "SELECT json_response FROM search WHERE search_id = $1 AND user_id = $2",
+      [params.id, userId]
     );
 
     if (result.rows.length === 0) {
       return NextResponse.json(
-        { error: "Search ID not found" },
+        { error: "Search ID not found or access denied" },
         { status: 404 }
       );
     }
