2b5c70efd54fa2cf83edb190df2d4f2850d56634
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-27 12:30:36 +0530

    Added Google Analytics scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
 import { Poppins } from "next/font/google";
 import ThemeWrapper from "../lib/ThemeWrapper";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["400", "700"], // Specify the weights you need
@@ -23,7 +23,32 @@ export default function RootLayout({
 }>) {
   return (
     <html className={poppins.variable} lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KPBH95CJ');
+            `,
+          }}
+        />
+      </head>
       <body className="font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KPBH95CJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeWrapper>{children}</ThemeWrapper>
       </body>
     </html>
